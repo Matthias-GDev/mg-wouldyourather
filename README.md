33ca04360f6ed9f4def8af4c24a53b87ce40a61d
Matthias-GDev <Matthias@Gutbrod.digital>
2021-01-28 20:31:23 +0100

    Add Tabs to Dashboard + Sort Questions, Update Readme

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # mg-wouldyourather
-Würdest du lieber" ist ein Konversations- oder Partyspiel, bei dem ein Dilemma in Form einer Frage gestellt wird, die mit "Würdest du lieber" beginnt. 
+
+# Short description
+
+German:
+"Würdest du lieber" ist ein Konversations- oder Partyspiel, bei dem ein Dilemma in Form einer Frage gestellt wird, die mit "Würdest du lieber" beginnt :)
+
+English:
+Would you rather" is a conversation or party game where a dilemma is posed in the form of a question starting with "Would you rather" :)
+
+## How to Install:
+
+- Open yout Terminal
+- Clone the git Project with "git clone https://github.com/Matthias-GDev/mg-wouldyourather.git"
+- Go into the folder "mg-wouldyourather"
+- Run `npm install` and install all project dependencies
+- Start the App with `yarn start` or `npm start`
+- View the App in your Browser: http://localhost:3000.
--- src/components/Dashbaord.js
+++ src/components/Dashbaord.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react'
 import '../css/dashboard.css';
 import { connect } from 'react-redux'
+import { Container,Row,Col,Card,Form,Button,Tabs,Tab } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { Redirect, withRouter } from 'react-router-dom'
 
 import Question from './Question'
@@ -9,22 +11,54 @@ class Dashboard extends Component {
 
     constructor(props){
         super(props)
+        this.state = {
+            countAnswered:0,
+            countUnanswered:0,
+        }
     }
 
     render(){
         const { questions, authedUser } = this.props
         const all_Questions = Object.values(questions)
 
+        const answeredQuestions = all_Questions.filter((q)=>{
+             const found = (q.optionOne.votes.indexOf(authedUser) > -1 || q.optionTwo.votes.indexOf(authedUser) > -1); 
+             return found}).sort((a, b) => b.timestamp - a.timestamp)
+        
+        const unansweredQuestions = all_Questions.filter((q)=>{ 
+            console.log("test: " + q.optionOne.votes.indexOf(authedUser) > 0)
+            const found = (q.optionOne.votes.indexOf(authedUser) > -1 || q.optionTwo.votes.indexOf(authedUser) > -1); 
+            return !found}).sort((a, b) => b.timestamp - a.timestamp)
+
         return(
-            <div>
-                <ul className="dashboard-card-list">
-                    {all_Questions.map((question) => (
-                        <li key={question.id}>
-                           <Question id={question.id} />
-                        </li>
-                    ))}
-                </ul>
-            </div>
+            <Container fluid>
+                <Row>
+                    <Col sm={3}></Col>
+                    <Col>
+                        <Tabs defaultActiveKey="unanswered" id="uncontrolled-tab-example">
+                             <Tab eventKey="unanswered" title={"Unanswered Questions ("+ unansweredQuestions.length +')'}>
+                                 <ul className="dashboard-card-list">
+                                    {unansweredQuestions.map((question) => (
+                                        <li key={question.id}>
+                                        <Question id={question.id} />
+                                        </li>
+                                    ))}
+                                </ul>
+                            </Tab>
+                             <Tab eventKey="answered" title={'Answered Questions (' + answeredQuestions.length+')'}>
+                                 <ul className="dashboard-card-list">
+                                    {answeredQuestions.map((question) => (
+                                        <li key={question.id}>
+                                        <Question id={question.id} />
+                                        </li>
+                                    ))}
+                                </ul>
+                            </Tab>
+                        </Tabs>
+                    </Col>
+                    <Col sm={3}></Col>
+                </Row>
+            </Container>
         )
     }
 }
